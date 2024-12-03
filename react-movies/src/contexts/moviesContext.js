d15495c3bb7ad15c3172c2a58559a1f2ebadf1db
rbirney <rbirney@wit.ie>
2024-12-03 15:29:02 +0000

    Added start files for CA2

---
+++ react-movies/src/contexts/moviesContext.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+export const MoviesContext = React.createContext(null);
+
+const MoviesContextProvider = (props) => {
+  const [favorites, setFavorites] = useState( [] )
+  const [myReviews, setMyReviews] = useState( {} ) 
+  const [playlist, setPlaylist] = useState( [] )
+
+  const addToFavorites = (movie) => {
+    let newFavorites = [];
+    if (!favorites.includes(movie.id)){
+      newFavorites = [...favorites, movie.id];
+    }
+    else{
+      newFavorites = [...favorites];
+    }
+    setFavorites(newFavorites)
+  };
+  
+  const addToPlaylist = (movie) => {
+    let newPlaylist = [];
+    if (!playlist.includes(movie.id)){
+      newPlaylist = [...playlist, movie.id];
+    }
+    else{
+      newPlaylist = [...playlist];
+    }
+    setPlaylist(newPlaylist)
+  };
+  //console.log(playlist)
+  
+  // We will use this function in a later section
+  const removeFromFavorites = (movie) => {
+    setFavorites( favorites.filter(
+      (mId) => mId !== movie.id
+    ) )
+  };
+  
+  const addReview = (movie, review) => {
+    setMyReviews( {...myReviews, [movie.id]: review } )
+  };
+  // console.log(myReviews);
+
+  return (
+    <MoviesContext.Provider
+      value={{
+        favorites,
+        playlist,
+        addToFavorites,
+        addToPlaylist,
+        removeFromFavorites,
+        addReview,
+      }}
+    >
+      {props.children}
+    </MoviesContext.Provider>
+  );
+};
+
+export default MoviesContextProvider;
---
+++ react-movies/src/pages/upcomingMovies.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist';
+
+const UpcomingPage = (props) => {
+
+  const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+
+  return (
+    <PageTemplate
+      title='Upcoming Movies'
+      movies={movies}
+      action={(movie) => {
+        return <AddToPlaylistIcon movie={movie} />
+      }}
+    />
+  );
+};
+export default UpcomingPage;
